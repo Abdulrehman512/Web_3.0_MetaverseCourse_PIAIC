e5ea0b35a46b8f200dcead405f707fa874f3afd5
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-24 23:37:34 +0500

    variables file added

---
+++ Course/Quarter 1/Practice/01 - Variables/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var myName = "Abdul";
+console.log(myName);
+var age = 21;
+console.log(age);
+var isHoliday = true;
+console.log(isHoliday);
+let firstName = "Abdul";
+let lastName = "Rehman";
+let fullName = ` Hi ${firstName} ${lastName}`; //Template Literals
+console.log(fullName);
---
+++ Course/Quarter 1/Practice/01 - Variables/index.ts
@@ -0,0 +1,13 @@
+var myName:string = "Abdul";
+console.log(myName);
+
+var age:number = 21;
+console.log(age);
+
+var isHoliday:boolean = true
+console.log(isHoliday);
+
+let firstName:string = "Abdul";
+let lastName:string = "Rehman";
+ let fullName:string =` Hi ${firstName} ${lastName}` //Template Literals
+console.log(fullName);
