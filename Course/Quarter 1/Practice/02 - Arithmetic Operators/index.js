bc3b39ec06266c3143573d3226172a8b5e378e2c
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-25 22:56:43 +0500

    arithmetic operators file added

---
+++ Course/Quarter 1/Practice/02 - Arithmetic Operators/index.js
@@ -0,0 +1,25 @@
+"use strict";
+//Addition
+let n = 5;
+let n1 = 7;
+console.log(n + n1);
+//Subtraction
+let m = 9;
+let m1 = 3;
+console.log(m - m1);
+//Multiplication
+let p = 4;
+let p1 = 2;
+console.log(p * p1);
+//Division
+let q = 8;
+let q1 = 2;
+console.log(q / q1);
+//Modulus
+let r = 10;
+let r1 = 3;
+console.log(r % r1);
+//Exponentiation
+let s = 6;
+let s1 = 2;
+console.log(s ** s1);
