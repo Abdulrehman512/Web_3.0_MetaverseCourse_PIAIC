cf781df4b71db40011c74f9e45daf0ca41ac8df2
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-27 23:32:17 +0500

    03 and 04 added

---
+++ Course/Quarter 1/Practice/03 - Assignment Operators/index.ts
@@ -0,0 +1,12 @@
+//Assignment Operators (used for assigning values to variables)
+
+let n:number = 11;
+console.log(n);
+
+n+=5; //n = n+5
+console.log(n);
+
+n-=5; n //n = n-5
+console.log(n);
+
+
---
+++ Course/Quarter 1/Practice/04 - Comparison Operators/index.ts
@@ -0,0 +1,24 @@
+// Comparison Operators (used to compare values of variables)
+
+let n:number = 7;
+
+console.log(n == 5); // checks that value of n is equal to 5
+// Outputs false because the value of n is not equal to 5, it's 7
+
+console.log(n === 5); 
+// Outputs false because even though the type and value are same for both operands but they are different data types.
+
+console.log(n != 5);
+//Output is true because n is not equal to 5
+
+console.log(n > 5);
+//Output is true because n is greater than 5
+
+console.log(n < 5);
+//Output is false because n is greater than 5
+
+console.log(n >= 5);
+//Output is true because n is greater or equal to 5
+
+console.log(n <= 5);
+//Output is false because n is greater than 5
