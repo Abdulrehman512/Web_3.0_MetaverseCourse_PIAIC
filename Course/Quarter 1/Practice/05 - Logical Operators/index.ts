2581639e6dc16729fa01f8eadae6058245b3543e
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-28 22:03:01 +0500

    logical operators added

---
+++ Course/Quarter 1/Practice/05 - Logical Operators/index.ts
@@ -0,0 +1,22 @@
+// Logical Operators (used to combine multiple conditions )
+
+let n:number = 5;
+
+console.log(n >= 5 && n < 10);
+//            T    &&    T     so Output is True
+
+console.log(n > 5 && n < 10);
+//            F    &&    T     so Output is False
+
+console.log(n >= 5 || n < 10);
+//            T    ||    T     so Output is True
+
+console.log(n > 5 || n < 10);
+//           F    ||    T     so Output is True
+
+console.log(!(n < 10));
+//          !T      so Output is False
+
+console.log(!(n > 10));
+//          !F      so Output is True
+
