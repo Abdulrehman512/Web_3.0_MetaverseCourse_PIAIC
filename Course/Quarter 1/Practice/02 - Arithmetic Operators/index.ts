586f148d0ed02c0c419789e5a010fe5a89849aac
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-28 22:15:42 +0500

    02

--- Course/Quarter 1/Practice/02 - Arithmetic Operators/index.ts
+++ Course/Quarter 1/Practice/02 - Arithmetic Operators/index.ts
@@ -9,22 +9,22 @@ let m: number = 9;
 let m1: number = 3;
 console.log(m - m1);
 
-//Multiplication
+//Multiplication.
 let p: number = 4;
 let p1: number = 2;
 console.log(p * p1);
 
-//Division
+//Division.
 let q: number = 8;
 let q1: number = 2;
 console.log(q/q1);
 
-//Modulus
+//Modulus.
 let r: number = 10;
 let r1: number = 3;
 console.log(r % r1);
 
-//Exponentiation
+//Exponentiation.
 let s: number = 6;
 let s1: number = 2;
 console.log(s ** s1);
