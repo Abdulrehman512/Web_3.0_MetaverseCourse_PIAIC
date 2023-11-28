6c03e3af43357e301bc7198161c986b14aaf42c6
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-28 22:08:11 +0500

    changes in 01

--- Course/Quarter 1/Practice/01 - Variables/index.ts
+++ Course/Quarter 1/Practice/01 - Variables/index.ts
@@ -1,13 +1,13 @@
-var myName:string = "Abdul";
+var myName:string = "Abdul"; //string means text.
 console.log(myName);
 
 var age:number = 21;
 console.log(age);
 
-var isHoliday:boolean = true
+var isHoliday:boolean = true //boolean means true or false.
 console.log(isHoliday);
 
 let firstName:string = "Abdul";
 let lastName:string = "Rehman";
- let fullName:string =` Hi ${firstName} ${lastName}` //Template Literals
+ let fullName:string =` Hi ${firstName} ${lastName}` //Template Literals.
 console.log(fullName);
