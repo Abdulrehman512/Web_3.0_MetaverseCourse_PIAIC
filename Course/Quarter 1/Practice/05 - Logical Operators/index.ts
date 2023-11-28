2a550405d07404556d53038a9cd779f221a9bcf7
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-28 22:06:15 +0500

    logical operators added

--- Course/Quarter 1/Practice/05 - Logical Operators/index.ts
+++ Course/Quarter 1/Practice/05 - Logical Operators/index.ts
@@ -1,22 +1,22 @@
-// Logical Operators are used to combine multiple conditions 
+// Logical Operators are used to combine multiple conditions. 
 
 let n:number = 5;
 
 console.log(n >= 5 && n < 10);
-//            T    &&    T     so Output is True
+//            T    &&    T     so Output is True.
 
 console.log(n > 5 && n < 10);
-//            F    &&    T     so Output is False
+//            F    &&    T     so Output is False.
 
 console.log(n >= 5 || n < 10);
-//            T    ||    T     so Output is True
+//            T    ||    T     so Output is True.
 
 console.log(n > 5 || n < 10);
-//           F    ||    T     so Output is True
+//           F    ||    T     so Output is True.
 
 console.log(!(n < 10));
-//          !T      so Output is False
+//          !T      so Output is False.
 
 console.log(!(n > 10));
-//          !F      so Output is True
+//          !F      so Output is True.
 
