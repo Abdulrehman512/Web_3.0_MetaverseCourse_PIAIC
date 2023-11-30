ba6eedbbacfe5a033e461a13508b1e56482e51ad
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-30 16:26:18 +0500

    07 added

--- Course/Quarter 1/Practice/07 - Nested-If-Else/index.ts
+++ Course/Quarter 1/Practice/07 - Nested-If-Else/index.ts
@@ -3,7 +3,7 @@ let age:number = 21;
 
 if (country === "Pakistan") {
     if (age >= 18) {
-        console.log("Here is your Ticket");
+        console.log("Here is your Ticket!");
         
     } else {
         console.log("Age Restriction");
