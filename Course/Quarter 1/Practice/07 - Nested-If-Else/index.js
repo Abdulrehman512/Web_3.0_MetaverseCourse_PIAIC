83a42ad55d77b0ad382c48b4c0b722eb9e8663f4
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-11-30 16:25:23 +0500

    07 added

---
+++ Course/Quarter 1/Practice/07 - Nested-If-Else/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let country = "Pakistan";
+let age = 21;
+if (country === "Pakistan") {
+    if (age >= 18) {
+        console.log("Here is your Ticket");
+    }
+    else {
+        console.log("Age Restriction");
+    }
+}
+else {
+    console.log("Invalid Country");
+}
---
+++ Course/Quarter 1/Practice/07 - Nested-If-Else/index.ts
@@ -0,0 +1,15 @@
+let country:string = "Pakistan";
+let age:number = 21;
+
+if (country === "Pakistan") {
+    if (age >= 18) {
+        console.log("Here is your Ticket");
+        
+    } else {
+        console.log("Age Restriction");
+        
+    }
+} else {
+    console.log("Invalid Country");
+    
+}
