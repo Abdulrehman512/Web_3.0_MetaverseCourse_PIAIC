702c08d06dabfc7cea83e041f426ccc18bac3420
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-12-01 22:38:47 +0500

    functions added

---
+++ Course/Quarter 1/Practice/08 - Functions/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function greet(name) {
+    console.log("Hello," + name.toUpperCase() + "!!");
+}
+greet("Abdul");
+function getFavoriteNumber() {
+    return 27;
+}
+getFavoriteNumber();
---
+++ Course/Quarter 1/Practice/08 - Functions/index.ts
@@ -0,0 +1,11 @@
+function greet(name:string) {
+    console.log("Hello," + name.toUpperCase() + "!!" );
+    
+}
+greet("Abdul")
+
+function getFavoriteNumber():number{
+    return 27;
+}
+
+getFavoriteNumber()
