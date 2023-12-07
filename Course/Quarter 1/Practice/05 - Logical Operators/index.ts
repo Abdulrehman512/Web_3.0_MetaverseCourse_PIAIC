57e6e957ffc104fa65d866d5b3d3a2d4e9d5be50
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-12-07 23:20:19 +0500

    comments added

--- Course/Quarter 1/Practice/05 - Logical Operators/index.ts
+++ Course/Quarter 1/Practice/05 - Logical Operators/index.ts
@@ -7,7 +7,8 @@ console.log(n >= 5 && n < 10);
 // so Output is True.
 
 console.log(n > 5 && n < 10);
-//            F    &&    T     so Output is False.
+//            F    &&    T     
+// so Output is False.
 
 console.log(n >= 5 || n < 10);
 //            T    ||    T     so Output is True.
