230294e17ca026f9651f890e5c42df852c8f5a4e
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-12-12 22:29:46 +0500

    array

---
+++ Course/Quarter 1/Practice/10 - Array/index.ts
@@ -0,0 +1,8 @@
+let fruits:string[] = ["Apple","Mango","Banana","Peach","WaterMelon"]
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
