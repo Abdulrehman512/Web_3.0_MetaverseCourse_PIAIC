3195e98c82bafe3a1596b68101b9f6b928866302
Abdulrehman512 <abdul.rehman11992@gmail.com>
2023-12-18 21:07:41 +0500

    04

--- Course/Quarter 1/Practice/04 - Comparison Operators/index.ts
+++ Course/Quarter 1/Practice/04 - Comparison Operators/index.ts
@@ -6,7 +6,7 @@ console.log(n == 5); // Checks that value of n is equal to 5.
 // Outputs false because the value of n is not equal to 5, it's 7.
 
 console.log(n === 5); 
-// Outputs false because even though the type and value are same for both operands but they are different data types.
+// Outputs false because even though the type and value are same for both operands but they are different data types..
 
 console.log(n != 5);
 //Output is true because n is not equal to 5
