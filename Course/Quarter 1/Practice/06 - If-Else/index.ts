03b949c4d0c30d4d061b00a6320b123e592b64ca
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-01-02 10:08:04 -0800

    6

--- Course/Quarter 1/Practice/06 - If-Else/index.ts
+++ Course/Quarter 1/Practice/06 - If-Else/index.ts
@@ -45,7 +45,7 @@ if (a > 0) {
     
 }
 
-// Q4: Write a Program that checks if a given Number is Even or odd.
+// Q4: Write a Program that checks if a given Number is Even or Odd.
 
 var b:number = 7;
 
