6a76141fad2f96f2e5e3097f58d71f0238f1f03d
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-01-07 08:57:06 -0800

    comments

--- Course/Quarter 1/Practice/10 - Array/index.ts
+++ Course/Quarter 1/Practice/10 - Array/index.ts
@@ -1,7 +1,7 @@
 let fruits:string[] = ["Apple","Mango","Banana","Peach","WaterMelon"]
 
 console.log(fruits);
-// Returns all Elements in array
+// Returns all Elements in Array
 console.log(fruits[0]);
 console.log(fruits[1]);
 console.log(fruits[2]);
