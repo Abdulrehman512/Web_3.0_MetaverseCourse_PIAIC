af4d48cc3f13f447be2df0edd97ef93c76831053
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-01-10 08:25:58 -0800

    comments

--- Course/Quarter 1/Practice/10 - Array/index.ts
+++ Course/Quarter 1/Practice/10 - Array/index.ts
@@ -5,6 +5,7 @@ console.log(fruits);
 console.log(fruits[0]);
 // Returns First Element in Array.
 console.log(fruits[1]);
+// Returns all Elements in Array.
 console.log(fruits[2]);
 console.log(fruits[3]);
 console.log(fruits[4]);
